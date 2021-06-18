91a3638a0216b2a6d4595200b1ff9e89485e2b0f
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-18 13:12:18 +0200

    exercise #4 done.

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,17 @@
 // function declaration
 const nMultiplesOf = (number, limit) => {
 
+    const nMultiplesOfArray = []
+    let index = 1;
+
+    while(nMultiplesOfArray.length < limit){
+
+        nMultiplesOfArray.push(number * index);
+        index ++;
+    }
+
+    return nMultiplesOfArray
+
 }
 
 console.log(nMultiplesOf(2, 10))
