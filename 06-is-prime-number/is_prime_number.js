48537ad95d0a6e136f557d707faddf4e51476145
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-18 13:58:09 +0200

    execise #6 done.

--- 06-is-prime-number/is_prime_number.js
+++ 06-is-prime-number/is_prime_number.js
@@ -1,8 +1,34 @@
 // check if a number is prime or not
 const isPrime = number => {
 
+    let numberOfDivider = getPrimes (number)
+
+    console.log(`Dividers ${number}'s are : ${numberOfDivider} \n`)
+
+    if (numberOfDivider.length <= 2) {
+        return true;
+        
+    } else {
+        return false
+
+    }
+
 }
 
 const getPrimes = amount => {
-    
+
+    const numberOfDivider = [] 
+    for(let i=1 ; i<= amount; i ++){
+        if(amount % i == 0){
+            numberOfDivider.push(i)
+        }
+    }
+    return numberOfDivider;
 }
+
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(4))
+console.log(isPrime(5))
+console.log(isPrime(6))
+console.log(isPrime(40))
