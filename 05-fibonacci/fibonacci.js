b9c333e2d8a34dc7456af76318cf461eacc10b47
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-18 13:36:44 +0200

    exercise #5 done.

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -2,3 +2,22 @@
 const maxNumber = Math.ceil(Math.random()*200);
 let n1 = 0, n2 = 1, nextTerm;
 
+console.log( `Fibonacci Series of ${maxNumber} :  \n`)
+
+const getFibonacciSequence = (maxNumber) => {
+
+let i;
+const fib = []; 
+
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= maxNumber; i++) {
+  // Next fibonacci number = previous + one before previous
+  
+  fib[i] = fib[i - 2] + fib[i - 1];
+}
+    return fib
+
+} 
+
+console.log(getFibonacciSequence(maxNumber))
