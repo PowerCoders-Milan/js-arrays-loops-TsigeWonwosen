31fb716d24040dbb68c810670d6583f9f54ae731
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-18 12:36:29 +0200

    exercise #2 done.

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,14 @@
 // read carefully the instructions
+
+let myString = "i am wondwosen";
+
+let stringArray =  myString.split("");
+
+console.log("Array of Strings: \n" + stringArray)
+
+stringArray = stringArray.reverse()
+
+console.log("Reversed Array of String : \n" + stringArray)
+
+stringArray = stringArray.join("")
+console.log("Reversed and Joined String : \n" + stringArray)
