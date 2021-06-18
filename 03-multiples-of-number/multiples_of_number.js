e484d3001cd441cf5fd8a055ef309524d031a50f
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-18 12:56:52 +0200

    exercise #3 done.

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,23 @@
 // define your function here
+
+const multiplesOf = (number, limit) => {
+    let multiples = []
+    for(let i=1; i<= limit; i++){
+        multiples.push(number * i);
+    }
+
+    return multiples;
+}
+
+
+console.log(`Multiple of a 2 : \n`)
+const multiplesOfNumber1 = multiplesOf(2, 5)
+
+console.log(multiplesOfNumber1)
+
+console.log(`\n`)
+console.log(`Multiple of a 11 : \n`)
+
+const multiplesOfNumber2 = multiplesOf(11, 5)
+
+console.log(multiplesOfNumber2)
